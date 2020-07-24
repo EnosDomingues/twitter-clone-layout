9ab63d2e8f46b4cf5bcf813635bc9c73dcb5cdce
Enos Domingues <enosdomingues@gmail.com>
2020-07-24 20:48:28 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import  GlobalStyles from './styles/GlobalStyles'; 
+import Layout from './components/Layout'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+       <Layout />
+
+      <GlobalStyles />
+    </>
   );
 }
 
